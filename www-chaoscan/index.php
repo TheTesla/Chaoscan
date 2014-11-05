b6c432bac1d9bf03dd629a2fd4c707a207bcc475
root <root@chchprsc.chch>
2014-11-06 00:15:47 +0100

    fixed little mistake

--- www-chaoscan/index.php
+++ www-chaoscan/index.php
@@ -1,7 +1,7 @@
 <html><head></head>
 <body>
 <h1>Chaoscan</h1>
-<a href="exit.php">-> Clean up, unlock the door and got out.</a><br><br>
+<a href="exit.php">-> Clean up, unlock the door and go out.</a><br><br>
 <a href="delall.php">delete all</a><br><br>
 <a href=download.php>download all scans</a><br><br>
 <?php
